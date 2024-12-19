80b21efd61544f66814f534913e05fe7cd42a748
kweon <kweon@221.162.102.112>
2024-12-19 21:52:11 +0900

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // "Hello, World!" 출력
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        
     }
 }
